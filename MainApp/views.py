ef49f911f6e4897c9d7c1cbcc9e2fefefc8d304d
Timur Zhamaletdinov <tim.zhamal@mail.ru>
2023-10-11 15:22:50 +0300

    snippets forms

--- MainApp/views.py
+++ MainApp/views.py
@@ -1,17 +1,45 @@
 from django.http import Http404
 from django.shortcuts import render, redirect
+from MainApp.forms import SnippetForm
+from MainApp.models import Snippet
 
 
 def index_page(request):
     context = {'pagename': 'PythonBin'}
     return render(request, 'pages/index.html', context)
 
-
 def add_snippet_page(request):
-    context = {'pagename': 'Добавление нового сниппета'}
-    return render(request, 'pages/add_snippet.html', context)
+    # Получаем чистую форму для заполнения
+    if request.method == "GET":
+        form = SnippetForm()
+        context = {
+            'pagename': 'Добавление нового сниппета',
+            'form': form
+        }
+        return render(request, 'pages/add_snippet.html', context)
+    # Создаем новый Сниппет(данные от формы)
+    if request.method == "POST":
+        form = SnippetForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("snippets-list")
+        return render(request, 'pages/add_snippet.html', {'form': form})
+
 
 
 def snippets_page(request):
-    context = {'pagename': 'Просмотр сниппетов'}
+    snippets = Snippet.objects.all()
+    context = {
+        'pagename': 'Просмотр сниппетов',
+        'snippets': snippets
+        }
     return render(request, 'pages/view_snippets.html', context)
+
+
+def snippet_detail(request, snippet_id):
+    snippet = Snippet.objects.get(id=snippet_id)
+    context = {
+        'pagename': 'Просмотр сниппетов',
+        'snippet': snippet
+        }
+    return render(request, 'pages/snippet_detail.html', context)
