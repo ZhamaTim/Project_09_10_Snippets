b7aaeaa135e56b3b24a0d844f9515eb6d7506567
Timur Zhamaletdinov <tim.zhamal@mail.ru>
2023-10-12 16:00:08 +0300

    edit

--- MainApp/views.py
+++ MainApp/views.py
@@ -2,7 +2,8 @@ from django.http import Http404
 from django.shortcuts import render, redirect
 from MainApp.forms import SnippetForm
 from MainApp.models import Snippet
-
+from django.core.exceptions import ObjectDoesNotExist
+from django.contrib import auth
 
 def index_page(request):
     context = {'pagename': 'PythonBin'}
@@ -21,8 +22,11 @@ def add_snippet_page(request):
     if request.method == "POST":
         form = SnippetForm(request.POST)
         if form.is_valid():
-            form.save()
-            return redirect("snippets-list")
+            snippet = form.save(commit=False)
+            if request.user.is_authenticated:
+                snippet.user = request.user
+                snippet.save()
+                return redirect("snippets-list")
         return render(request, 'pages/add_snippet.html', {'form': form})
 
 
@@ -37,9 +41,62 @@ def snippets_page(request):
 
 
 def snippet_detail(request, snippet_id):
-    snippet = Snippet.objects.get(id=snippet_id)
+    try:
+        snippet = Snippet.objects.get(id=snippet_id)
+    except ObjectDoesNotExist:
+        raise Http404
     context = {
         'pagename': 'Просмотр сниппетов',
-        'snippet': snippet
+        'snippet': snippet,
+        'type': 'view'
         }
     return render(request, 'pages/snippet_detail.html', context)
+
+
+def snippet_delete(request, snippet_id):
+    snippet = Snippet.objects.get(id=snippet_id)
+    snippet.delete()
+    return redirect("snippets-list")
+
+def snippet_edit(request, snippet_id):
+    try:
+        snippet = Snippet.objects.get(id=snippet_id)
+    except ObjectDoesNotExist:
+        raise Http404
+    # Получаем страницу с данными сниппета
+    if request.method == "GET":
+        context = {
+            'pagename': 'Просмотр сниппетов',
+            'snippet': snippet,
+            'type': 'edit'
+            }
+        return render(request, 'pages/snippet_detail.html', context)
+    # Изменяем атрибуты сниппета на основе данных из формы и сохраняем в базу
+    if request.method == "POST":
+        data_form = request.POST
+        snippet.name = data_form["name"]
+        snippet.code = data_form["code"]
+        snippet.creation_date = data_form["creation_date"]
+        snippet.save()
+        return redirect("snippets-list")
+
+
+def login(request):
+    if request.method == 'POST':
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+        user = auth.authenticate(request, username=username, password=password)
+        if user is not None:
+            auth.login(request, user)
+        else:
+            # Return error message
+            pass
+    return redirect('home')
+
+
+
+
+def logout(request):
+    auth.logout(request)
+    # реализуем перенаправление на ту страницу, на которой логинился пользователь
+    return redirect(request.META.get('HTTP_REFERER', '/'))
